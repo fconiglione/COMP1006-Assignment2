099bed93a52f5e116a85194785a20e67045159d9
fconiglione <faconigl@lakeheadu.ca>
2023-03-18 20:33:21 -0400

    Added a global header and footer

---
+++ content-manager.php
@@ -0,0 +1,8 @@
+<?php
+$title = 'Content Manager';
+require('includes/header-a.php');
+?>
+    <main>
+
+    </main>
+    <?php require('includes/footer-a.php'); ?>
---
+++ includes/footer-a.php
@@ -0,0 +1,5 @@
+<footer>
+        COMP1006 | PHP Content Managrr | &copy; <?php echo date("Y"); ?> Francesco Coniglione
+    </footer>
+</body>
+</html>
